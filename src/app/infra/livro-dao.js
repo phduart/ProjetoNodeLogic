ee6d792a67d16c115506a14f2591360020b1337c
phduart <49646944+phduart@users.noreply.github.com>
2022-07-29 17:23:15 -0300

    subindo projeto inicial

---
+++ src/app/infra/livro-dao.js
@@ -0,0 +1,44 @@
+class LivroDao {
+    constructor(db){
+        this._db = db;
+    }
+
+    lista() {
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if (erro) return reject('Não foi possível listar os livros');
+                    return resolve(resultados);
+                    }   
+                )
+        }); 
+    }
+
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO LIVROS (
+                        titulo,
+                        preco,
+                        descricao
+                    ) values (?, ?, ?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ], 
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+                    resolve();
+                } 
+            )        
+        });
+    }
+}
+
+module.exports = LivroDao;
